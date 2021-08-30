821d6cde1a9f16a8d3e3c31fc57b1aa2e59863b2
Lucas Souleyrasse <you@example.comlucas.souleyrasse@gmail.com>
2021-08-30 16:24:47 +0200

    test redo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import "./App.css";
+import GitUsers from "./components/gitusers";
+import Crypto from "./components/randomcrypto";
+import GetRandomNation from "./components/getrandomnation";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <nav>
+          <Link to="/gitusers">Github Users</Link>
+          <Link to="/randomcrypto">Random crypto</Link>
+          <Link to="/GetRandomNation">Random Nation</Link>
+        </nav>
+        <Route path="/gitusers" component={GitUsers} />
+        <Route path="/randomcrypto" component={Crypto} />
+        <Route path="/GetRandomNation" component={GetRandomNation} />
+      </Router>
     </div>
   );
 }
